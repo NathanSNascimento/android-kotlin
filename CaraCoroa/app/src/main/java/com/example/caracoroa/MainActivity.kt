8e5b4ee5db33ef750c026271b2738a6718e9fe88
NathaNascimento <nathansantos3211@gmail.com>
2019-04-03 21:21:45 -0300

    Projeto finalizado Cara e Coroa.

---
+++ CaraCoroa/app/src/main/java/com/example/caracoroa/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.caracoroa
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button_jogar.setOnClickListener {
+
+            val intent = Intent(this, ResultadoActivity::class.java)
+
+            val numSorteado = Random().nextInt(2);
+
+            intent.putExtra("numero:", numSorteado)
+
+            startActivity(intent)
+        }
+    }
+}
---
+++ CaraCoroa/app/src/main/java/com/example/caracoroa/ResultadoActivity.kt
@@ -0,0 +1,27 @@
+package com.example.caracoroa
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_resultado.*
+
+class ResultadoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resultado)
+
+        val dados: Bundle = intent.extras
+        val numSorteado = dados.getInt("numero")
+
+        if (numSorteado == 0 ){
+            image_result.setImageResource(R.drawable.moeda_cara)
+        }else{
+            image_result.setImageResource(R.drawable.moeda_coroa)
+        }
+
+        button_voltar.setOnClickListener {
+
+            finish()
+        }
+    }
+}
